f08666934b114575447b57d2cbcc62b8b6b290f7
Mike Broadfoot <mbroadfo@yahoo.com>
2025-04-27 21:27:38 -0600

    Override log level

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 handler.setFormatter(formatter)
 
