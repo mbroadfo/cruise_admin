0de19b1fa313000785aa27c36265ca1da5068e7d
Mike Broadfoot <mbroadfo@yahoo.com>
2025-11-28 16:15:42 -0700

    Remove unused ECR variables from Terraform
    - Removed ecr_repo_name variable (no longer needed for ZIP deployment)
    - Removed image_tag variable (no longer needed for ZIP deployment)
    - All ECR resources and references cleaned up

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,18 +10,6 @@ variable "app_name" {
   default     = "cruise-admin-api"
 }
 
-variable "ecr_repo_name" {
-  description = "ECR Repository name"
-  type        = string
-  default     = "cruise-admin-api"
-}
-
-variable "image_tag" {
-  description = "Image tag to deploy"
-  type        = string
-  default     = "latest"
-}
-
 variable "auth0_domain" {
   description = "Auth0 Domain (without https://)"
   type        = string
